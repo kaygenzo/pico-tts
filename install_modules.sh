0f025b536a1f83c462f726f2c342f9541c0fad0d
Karim Yarboua <karim.yarboua@gmail.com>
2023-02-26 15:42:59 +0100

    Add a script bash to install all submodules

---
+++ install_modules.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git submodule add -b master https://android.googlesource.com/platform/external/svox
+git submodule add -b master https://android.googlesource.com/platform/frameworks/base
+git submodule add -b master https://android.googlesource.com/platform/libnativehelper
+git submodule add -b master https://android.googlesource.com/platform/system/core
+git submodule add -b master https://android.googlesource.com/platform/system/logging
+git submodule add -b master https://android.googlesource.com/platform/external/expat
+git submodule add -b lineage-20.0 https://github.com/LineageOS/android_hardware_lineage_compat.git
